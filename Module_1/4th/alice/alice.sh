792f0d77148912cc0b3fe1e2c557601e14b0b695
ddtemp <daniyarb331@gmail.com>
2023-10-20 17:00:44 +0700

    Init commit

--- Module_1/4th/alice/alice.sh
+++ Module_1/4th/alice/alice.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-book_file=$(cat "/home/ddtemp/Desktop/Eltex/4th/alice/alice.txt")
+book_file="/home/ddtemp/Desktop/Eltex/Eltex_courses/Module_1/4th/alice/alice.txt"
 
-letters=$(echo $book_file | sed 's/[^[:alpha:]]//g' | wc -m)
-words=$(echo $book_file | wc -w)
-first_mention=$(echo $book_file | sed -n '/[.!?/{p;q;}' "$book_file" | grep -i 
-королева)
+# Задача 1: Вывести количество букв в произведении
+letters_count=$(cat "$book_file" | sed -e 's/[^[:alpha:]]//g' | wc -m)
 
-echo "Amount of letters in the text: $letters"
-echo "Amount of words in the text: $words"
-echo "First queen's mention: $first_mention"
+# Задача 2: Вывести количество слов в произведении
+words_count=$(cat "$book_file" | sed -e 's/[^[:alpha:]]/ /g' | tr -s ' ' | wc -w)
 
+# Задача 3: Вывести первое предложение, в котором упоминается королева
+queen_sentence=$(sed -n '/[.!?]/{s/.*королев[а-я]*[.!?].*/&/p;q}' "$book_file")
+
+# Задача 4: Вывести всех участников чаепития
+tea_party_participants=$(sed -n '/чаепитие/,$s/[^[:alpha:]]/ /g;p' "$book_file" | grep -o -w '\w\+' | sort | uniq)
+
+echo "Количество букв в произведении: $letters_count"
+echo "Количество слов в произведении: $words_count"
+echo "Первое упоминание королевы: $queen_sentence"
+echo "Участники чаепития: $tea_party_participants"
