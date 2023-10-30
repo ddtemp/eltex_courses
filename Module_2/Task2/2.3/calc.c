d93a659a02b5b8c972843b35b39420521f114cb0
Dan <daniyarb331@gmail.com>
2023-10-30 20:45:35 +0700

    lib for calculator

--- Module_2/Task2/2.3/calc.c
+++ Module_2/Task2/2.3/calc.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 
-double addition(double num_1, double num_2) {
-	return num_1 + num_2;
+double addition(double num_1, double num_2) { return num_1 + num_2; }
+
+double subtraction(double num_1, double num_2) { return num_1 - num_2; }
+
+double multiplication(double num_1, double num_2) { return num_1 * num_2; }
+
+double division(double num_1, double num_2) {
+  if (num_2 != 0)
+    return num_1 / num_2;
+  else {
+    printf("\nError. Division by zero\n");
+    return 0;
+  }
 }
 
-double subtraction(double num_1, double num_2){
-	return num_1 - num_2;
-}
+double exponentiation(double num, double exponent) {
+  double result = 1.0;
 
-double multiplication(double num_1, double num_2){
-	return num_1 * num_2;
-}
+  if (exponent > 0) {
+    for (int i = 0; i < exponent; ++i) {
+      result *= num;
+    }
+  } else if (exponent < 0) {
+    for (int i = 0; i > exponent; --i) {
+      result /= num;
+    }
+  }
 
-double division(double num_1, double num_2){
-	if (num_2 != 0) return num_1 / num_2;
-	else {
-		printf("\nError. Division by zero\n");
-		return 0;
-	}
-}
-
-double exponentiation(double num, double exponent){
-	double result = 1.0;
-
-	if (exponent > 0) {
-		for (int i = 0; i < exponent; ++i){
-			result *= num;
-		}
-	} else if (exponent < 0) {
-		for (int i = 0; i > exponent; --i){
-			result /= num;
-		}
-	}
-	
-	return result;
+  return result;
 }
